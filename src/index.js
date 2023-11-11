1728a277f6bf98af74b31ef936d6ce92176798ea
Josh Wang <Joshw9012@gmail.com>
2023-11-11 23:43:43 +1100

    C10 - 119 passing props/ default  to a component - Get rating data to an external component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StartRating from "./StarRating";
 
+// passing rating number from a component to external component
+function GetRatingData() {
+  const [getRating, setGetRating] = useState(0);
+
+  return (
+    <>
+      <StartRating color="blue" maxRating={10} onSetRating={setGetRating} />
+      <p>This movie was rated {getRating} stars</p>
+    </>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StartRating maxRating={5} />
+    <StartRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "perfect"]}
+      defaultRating={3}
+    />
+    <GetRatingData />
   </React.StrictMode>
 );
